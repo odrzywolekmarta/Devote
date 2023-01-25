b64d9725f3c729e1d910a442ded0d7ccdae3a63b
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-01-25 13:18:03 +0100

    initial commit

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Majka on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
