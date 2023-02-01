92d4b11303cc62d62b3357b9fb1c86a592d04e4f
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-02-01 15:26:42 +0100

    do minor refactor

---
+++ Devote/Devote/Utility/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Devote
+//
+//  Created by Majka on 01/02/2023.
+//
+
+import SwiftUI
+
+//MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
