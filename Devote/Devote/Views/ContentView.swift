113b1b2a6eae2706f496e6b412d4afcdfb62d764
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-02-21 20:06:16 +0100

    add header

--- Devote/Devote/Views/ContentView.swift
+++ Devote/Devote/Views/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     //MARK: - PROPERTIES
     @State var task: String = ""
     @State private var showNewTaskItem: Bool = false
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     
     //MARK: - FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -43,6 +44,36 @@ struct ContentView: View {
                 //MARK: - MAIN VIEW
                 VStack {
                 //MARK: - HEADER
+                    HStack(spacing: 10) {
+                        // TITLE
+                        Text("Devote")
+                            .font(.system(.largeTitle, design: .rounded))
+                            .fontWeight(.heavy)
+                            .padding(.leading, 4)
+                        
+                        Spacer()
+                        
+                        // EDIT BUTTON
+                        EditButton()
+                            .font(.system(size: 14, weight: .semibold, design: .rounded))
+                            .padding(.horizontal, 10)
+                            .frame(minWidth: 70, minHeight: 24)
+                            .background(
+                                Capsule().stroke(Color.white, lineWidth: 2)
+                            )
+                        // APPEARANCE BUTTON
+                        Button {
+                            isDarkMode.toggle()
+                        } label: {
+                            Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .font(.system(.title, design: .rounded))
+                        }
+
+                    } // hstack
+                    .padding()
+                    .foregroundColor(.white)
                     Spacer(minLength: 80)
                 //MARK: - NEW TASK BUTTON
                     Button {
@@ -94,12 +125,7 @@ struct ContentView: View {
             } // zstack
             .scrollContentBackground(.hidden)
             .navigationTitle("Daily Tasks")
-            .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-            } // toolbar
+            .toolbar(.hidden)
             .background(
             BackgroundImageView()
             )
