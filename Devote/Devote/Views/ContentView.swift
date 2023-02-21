556e4545690ab9d3ec76e89c7d1d5045ea10c953
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-02-21 12:01:32 +0100

    refactor ui

--- Devote/Devote/Views/ContentView.swift
+++ Devote/Devote/Views/ContentView.swift
@@ -11,6 +11,7 @@ import CoreData
 struct ContentView: View {
     //MARK: - PROPERTIES
     @State var task: String = ""
+    @State private var showNewTaskItem: Bool = false
     
     //MARK: - FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -21,9 +22,6 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
     
     //MARK: - FUNCTIONS
-    
-   
-    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
@@ -42,7 +40,28 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                //MARK: - MAIN VIEW
                 VStack {
+                //MARK: - HEADER
+                    Spacer(minLength: 80)
+                //MARK: - NEW TASK BUTTON
+                    Button {
+                        showNewTaskItem = true
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 30, weight: .semibold, design: .rounded))
+                        Text("New task")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                            .clipShape(Capsule())
+                            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 0.0, y: 4.0)
+                    )
+                //MARK: - TASKS
                     List {
                         ForEach(items) { item in
                             NavigationLink {
@@ -62,6 +81,11 @@ struct ContentView: View {
                     .listStyle(InsetGroupedListStyle())
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
                 } // vstack
+            //MARK: - NEW TASK ITEM
+                if showNewTaskItem {
+                    BlankView()
+                    NewTaskItemView()
+                } 
             } // zstack
             .scrollContentBackground(.hidden)
             .navigationTitle("Daily Tasks")
